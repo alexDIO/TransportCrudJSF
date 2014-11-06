21aa0fb24823986d2404606384b977e6e9841ee9
Alex.DIO <olomakovskyi@dio-soft.com>
2014-11-06 18:48:28 +0200

    started implementation of jsf crud

---
+++ src/main/java/jsf/JSFTransportManager.java
@@ -0,0 +1,94 @@
+package jsf;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * Created by olomakovskyi on 11/6/2014.
+ */
+
+@ManagedBean(name = "manager", eager = true)
+public class JSFTransportManager {
+    private int id;
+    private String transportType;
+    private String mark;
+    private String color;
+    private int manufactureYear;
+    private int passengersCount;
+    private String energySource;
+    private String transmission;
+    private int load;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTransportType() {
+        return transportType;
+    }
+
+    public void setTransportType(String transportType) {
+        this.transportType = transportType;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public void setManufactureYear(int manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    public int getPassengersCount() {
+        return passengersCount;
+    }
+
+    public void setPassengersCount(int passengersCount) {
+        this.passengersCount = passengersCount;
+    }
+
+    public String getEnergySource() {
+        return energySource;
+    }
+
+    public void setEnergySource(String energySource) {
+        this.energySource = energySource;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void setLoad(int load) {
+        this.load = load;
+    }
+}
